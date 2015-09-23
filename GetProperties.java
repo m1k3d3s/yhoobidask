1efe176a5482adb5667c73026696868a4bf902e8
m1k3d3s <mikedes@gmail.com>
2015-09-23 12:07:28 -0400

    adding properties to get - url , log file location

--- GetProperties.java
+++ GetProperties.java
@@ -3,27 +3,44 @@ import java.io.IOException;
 import java.io.FileInputStream;
 
 public class GetProperties {
-	Properties prop = new Properties();
-	String stock = null;
-	{
-		try{
-			prop.load(new FileInputStream("/home/mikedes/workspace/JSoupParser/bidask.config"));
-		}catch(IOException e){
-			System.out.println(e);
-		}
-		
-		for(String key : prop.stringPropertyNames()) {
-			String value = prop.getProperty(key);
-			if(value != null) {
-				stock = value;
-			} else {
-				System.out.println("No stock value set.");
-			}
-		}
-	}
-	
-	public String selectStock() {
-		return stock;
-	}
-	
+        Properties prop = new Properties();
+        String stock = null;
+        String url = null;
+        String logfile = null;
+        {
+                try{
+                        prop.load(new FileInputStream("bidask.config"));
+                }catch(IOException e){
+                        System.out.println(e);
+                        System.out.println("Unable to continue.");
+                }
+                
+                for(String key : prop.stringPropertyNames()) {
+                        String value = prop.getProperty(key);
+                        if(key.matches("stock") && value != null) {
+                                stock = value;
+                                System.out.println(stock);
+                        }else if(key.matches("url") && value != null) {
+                                url = value;
+                                System.out.println(url);
+                        }else if(key.matches("logfile") && value != null){
+                                logfile=value;
+                        } else {
+                                System.out.println("Unknown key/value pair. Skipping.");
+                                System.out.println(key + " " + value);
+                        }
+                }
+        }
+        
+        public String selectStock() {
+                return stock;
+        }
+        public String selectUrl(){
+                return url;
+        }
+        public String selectLogFile(){
+                return logfile;
+        }
+        
 }
+
